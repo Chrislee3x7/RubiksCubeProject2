f2a8649616e6eb156a5da755cb7e574ff20a0476
Chrislee3x7 <chrislee3x7@gmail.com>
2020-05-14 19:20:02 -0700

    fixed errors

--- RubiksCubeProject/src/RubiksCube.java
+++ RubiksCubeProject/src/RubiksCube.java
@@ -20,35 +20,28 @@ public class RubiksCube {
         window = new JFrame("Rubik's Cube");
 
         DisplayCube displayCube = new DisplayCube(cube);
-<<<<<<< HEAD
         ControlPanel controlPanel = new ControlPanel();
 
         window.add(displayCube, BorderLayout.WEST);
         window.add(controlPanel, BorderLayout.EAST);
-=======
         window.getContentPane().add(displayCube);
->>>>>>> 169a18cfe1e46d1addac9ebb7542185ec553f2cb
 
-        //window.setBounds(200, 50, 400, 430);
-        window.setBounds(200, 50, 600, 650);
+        window.setBounds(50, 50, 600, 650);
+        window.pack();
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setResizable(false);
         window.setVisible(true);
 
         displayCube.update();
 
-<<<<<<< HEAD
         cube.rotateSideLayer(1,TurnDirection.CLOCKWISE);
         //cube.rotateM(TurnDirection.CLOCKWISE, LayerNotation.M);
 
-=======
         cube.rotateSideLayer(2,TurnDirection.COUNTERCLOCKWISE);
         cube.rotateSideLayer(2,TurnDirection.COUNTERCLOCKWISE);
         cube.rotateSideLayer(0, TurnDirection.CLOCKWISE);
         cube.rotateSideLayer(0, TurnDirection.CLOCKWISE);
         displayCube.update();
->>>>>>> 169a18cfe1e46d1addac9ebb7542185ec553f2cb
-
         //cube.rotateSideLayer(0, TurnDirection.CLOCKWISE);
     }
 
