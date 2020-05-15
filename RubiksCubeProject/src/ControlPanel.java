a843cd7e90f02a2bb4c7841cbc4650c059e24789
Chrislee3x7 <chrislee3x7@gmail.com>
2020-05-14 23:11:17 -0700

    fixed initialization of buttons

--- RubiksCubeProject/src/ControlPanel.java
+++ RubiksCubeProject/src/ControlPanel.java
@@ -12,37 +12,18 @@ public class ControlPanel extends JPanel {
 
     private GridLayout gridLayout;
 
+    private String[] allNotation = {"U", "L", "F", "U'", "L'", "F'", "R", "B", "D", "R'", "B'", "D'",
+            "M", "S", "E", "M'", "S'", "E'", "x", "y", "z", "x'", "y'", "z'"};
+
     public ControlPanel(Cube cube) {
         setPreferredSize(new Dimension(WIDTH, HEIGHT + 50));
         gridLayout = new GridLayout(8,3);
         setLayout(gridLayout);
 
         this.cube = cube;
-
-        add(new ControlButton(this, "U"));
-        add(new ControlButton(this, "L"));
-        add(new ControlButton(this, "F"));
-        add(new ControlButton(this, "U'"));
-        add(new ControlButton(this, "L'"));
-        add(new ControlButton(this, "F'"));
-        add(new ControlButton(this, "R"));
-        add(new ControlButton(this, "B"));
-        add(new ControlButton(this, "D"));
-        add(new ControlButton(this, "R'"));
-        add(new ControlButton(this, "B'"));
-        add(new ControlButton(this, "D'"));
-        add(new ControlButton(this, "M"));
-        add(new ControlButton(this, "S"));
-        add(new ControlButton(this, "E"));
-        add(new ControlButton(this, "M'"));
-        add(new ControlButton(this, "S'"));
-        add(new ControlButton(this, "E'"));
-        add(new ControlButton(this, "x"));
-        add(new ControlButton(this, "y"));
-        add(new ControlButton(this, "z"));
-        add(new ControlButton(this, "x'"));
-        add(new ControlButton(this, "y'"));
-        add(new ControlButton(this, "z'"));
+        for (String notationSymbol : allNotation) {
+            add(new ControlButton(this, notationSymbol));
+        }
     }
 
     public void sendCommand(String commandNotation) {
