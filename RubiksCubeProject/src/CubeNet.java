6fe485e3704cc92bd803c7bbf9b17f22da6dfb30
Alexlee8888 <65263737+Alexlee8888@users.noreply.github.com>
2020-05-16 13:47:29 -0700

    created tools class

---
+++ RubiksCubeProject/src/CubeNet.java
@@ -0,0 +1,8 @@
+public class CubeNet {
+
+    private Cube cube;
+
+    public CubeNet() {
+        this.cube = new Cube();
+    }
+}
---
+++ RubiksCubeProject/src/Tools.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class Tools extends JComponent {
+    
+}
