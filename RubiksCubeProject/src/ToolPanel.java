e137e82016c294ea1fafd69e47a5b0be2725676f
Chrislee3x7 <chrislee3x7@gmail.com>
2020-05-19 13:00:16 -0700

    pooo

--- RubiksCubeProject/src/ToolPanel.java
+++ RubiksCubeProject/src/ToolPanel.java
@@ -13,13 +13,10 @@ public class ToolPanel extends JPanel {
 
         isVisible = false;
         setLayout(new BorderLayout());
-<<<<<<< HEAD
-        cubeNet = new CubeNetPanel(cube.getCubeFaceArray());
-        leftToolsPanel = new LeftToolsPanel(cube.getDisplayCube());
-=======
         cubeNet = new CubeNetPanel(cube);
-        changeViewPanel = new ChangeViewPanel(cube.getDisplayCube());
->>>>>>> parent of d9a2aa8... pp
+        leftToolsPanel = new LeftToolsPanel(cube.getDisplayCube());
+        cubeNet = new CubeNetPanel(cube);
+        leftToolsPanel = new LeftToolsPanel(cube.getDisplayCube());
         add(cubeNet, BorderLayout.EAST);
         add(leftToolsPanel, BorderLayout.WEST);
         //setBorder(BorderFactory.createLineBorder(Color.GREEN));
